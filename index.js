fa54c6190e15423e300c1d07039ff364325c7427
vng114 <vng114@ukr.net>
2024-03-17 14:26:53 +0200

    add await functions

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 require('dotenv').config();
 
+
+main();
+
 console.log(process.env.msgGreeting);
+
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve,ms));
+}
+
+async function main() {
+    console.log("Before sleep");
+    //await sleep(10000); 
+    await makeLoop(1000);
+    
+    await makeLoop(1500);
+    console.log("After sleep");
+}
+
+async function makeLoop(ms) {
+    let counter = 1;
+    while (1) {
+        await sleep(ms);
+        console.log(counter++)
+    }
+
+
+    
+}
